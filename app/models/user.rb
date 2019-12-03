31e5381e549b913776add5544eadcc563317209c
Juyeong Lee <2jy22@naver.com>
2019-12-03 16:15:51 +0900

    5-1 : N:N 관계

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts # posts라는 메서드가 활성화됨, 반드시 복수로 써 줄 것
+  has_many :likes
+  has_many :liked_posts, through: :likes, source: :post # likes를 통해서 user가 좋아하는 post를 모두 가져와라
+  # user 입장에서 좋아하는 글들을 가져오는데 그걸 찾아올 때 likes를 토대로 찾게되고 likes 속에서 post인 애들만 묶음으로 보여준다는 의미
+
 end
