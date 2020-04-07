2cfd24e13b156cb49671f463bf7c6b963c6306ae
Juyeong Lee <2jy22@naver.com>
2020-04-07 23:24:56 +0900

    6-3 : User 아바타 이미지

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -23,6 +23,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
+  process :resize_to_limit => [512, 512]
   #
   # def scale(width, height)
   #   # do something
@@ -32,6 +33,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
+  version :thumb do
+    process :resize_to_limit => [32, 32]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
